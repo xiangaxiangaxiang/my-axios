ad3e2dba81ca532236be115e9bf72b32cccd57b9
xiangaxiangaxiang <872632899@qq.com>
2021-03-23 22:44:53 +0800

    transform data

--- src/my-axios.ts
+++ src/my-axios.ts
@@ -1,5 +1,25 @@
+import { transformRequest } from './helpers/data'
+import { buildUrl } from './helpers/url'
 import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
 
-function axios(config: AxiosRequestConfig) {}
+function axios(config: AxiosRequestConfig) {
+  processConfig(config)
+  xhr(config)
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildUrl(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+  config.data = transformRequestData(config)
+}
 
 export default axios
